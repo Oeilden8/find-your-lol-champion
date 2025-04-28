16713dd36b4c88a7e0082bf46d9db0cd4bcf14de
Fanny <oeilden8@gmail.com>
2025-04-28 15:20:23 +0200

    added loading component and vite conf for typescript

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import loading from '../assets/icons/loading-buffer.gif';
 
 function Loading() {
   return (
@@ -11,7 +11,7 @@ function Loading() {
         alignItems: 'center',
       }}
     >
-      Loading
+      <img src={loading} alt='icon indicating loading' width={50} />
     </div>
   );
 }
